0ce943429de691a0789499cd10eba16b6700becb
SudhakarGVVN <sudhakar@zanjo.io>
2017-07-04 08:28:56 +0530

    Desning complete

--- JSON Downloading Into PickerView/ViewController.swift
+++ JSON Downloading Into PickerView/ViewController.swift
@@ -9,17 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var view_ContainerView: UIView!
+    
+    @IBOutlet weak var view_PickerView: UIPickerView!
+    
+    @IBOutlet weak var activity1: UIActivityIndicatorView!
+    
+    @IBOutlet weak var activity2: UIActivityIndicatorView!
 
+    @IBOutlet weak var lbl_ShowSelectItem: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    @IBAction func btn_ChooseDays(_ sender: Any) {
     }
 
+    @IBAction func btn_ChooseLocation(_ sender: Any) {
+    }
 
+    @IBAction func btn_SelectItems(_ sender: Any) {
+    }
 }
 
